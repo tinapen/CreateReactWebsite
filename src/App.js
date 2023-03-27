de4ccbc0525b248649d69219d3677f79be8a7ccf
TinaPen <cristinapenaflor20@gmail.com>
2023-03-28 06:09:47 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles.css";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/services" exact element={<Services />} />
+          <Route path="/sign-up" exact element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+export default App;
