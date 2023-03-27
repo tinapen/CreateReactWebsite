b4af4aef2764ef0b2e701ab1ebb074c861ca009f
TinaPen <cristinapenaflor20@gmail.com>
2023-03-28 06:06:47 +0800

    Add files via upload

---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../styles.css";
+import Cards from "../Cards";
+import HeroSection from "../HeroSection";
+import Footer from "../Footer";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
