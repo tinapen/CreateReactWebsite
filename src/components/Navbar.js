0ae29759330eec096d0ef9faf6e22b08389dfbf7
TinaPen <cristinapenaflor20@gmail.com>
2023-03-28 03:24:42 +0000

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,12 +40,20 @@ function Navbar() {
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 Services
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 Products
               </Link>
             </li>
