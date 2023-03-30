e0feb9178df6d1a8b47fec720a2526fad4f45ee0
TinaPen <cristinapenaflor20@gmail.com>
2023-03-30 03:12:26 +0000

    update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
         <div className="cards_wrapper">
           <ul className="cards_items">
             <CardItem
-              src="/public/images/img-9.jpg"
+              src="./images/img-9.jpg"
               text="Explore the hidden waterfall deep insidee the Amazon Jungle"
               label="Adventure"
               path="/services"
             />
             <CardItem
-              src="/public/images/img-2.jpg"
+              src="./images/img-2.jpg"
               text="Travel through the Islands of Bali in a Private Cruise"
               label="Luxury"
               path="/services"
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className="cards_items">
             <CardItem
-              src="/public/images/img-3.jpggg"
+              src="./images/img-3.jpg"
               text="Sea Sail in the Atlantic Ocean visiting Uncharted Waters"
               label="Mystery"
               path="/services"
             />
             <CardItem
-              src="/public/images/img-4.jpg"
+              src="./images/img-4.jpg"
               text="Experience Football on Top of the Himalayan Mountains"
               label="Adventure"
               path="/services"
             />
             <CardItem
-              src="/public/images/img-8.jpg"
+              src="./images/img-8.jpg"
               text="Rides through the Sahara Dessert on a guided camel tour"
               label="Adventure"
               path="/services"
